b6ed86a7abc9de4f2798ec7d88fca5a23eaaf9ab
Clément Prod'homme <prodhomme.clement@hotmail.fr>
2015-10-22 12:16:24 +0200

    WIP save product check

--- app/controllers/makeShop.js
+++ app/controllers/makeShop.js
@@ -2,6 +2,7 @@
 // On set la liste des produits
 var listProducts = [
 	{
+		id : "switch_product",
 		checked: {
 			myID : 0,
 			value : false
@@ -60,16 +61,32 @@ var listProducts = [
 
 //var allProducts = Ti.App.Properties.setObject('allProducts', listProducts);
 var allProducts = Ti.App.Properties.getObject('allProducts');
-console.log(allProducts);
+//console.log(allProducts);
 if (allProducts == null) {
 	allProducts = Ti.App.Properties.setObject('allProducts', listProducts);
 };
 $.elementsList.sections[0].setItems(allProducts);
 
 function doSwitchMe(e) {
-    console.log(e);
+    for (var key in allProducts) {
+		if (key == e['itemIndex']) {
+			if (allProducts[key].checked.value == true) {
+				allProducts[key].checked.value = false;
+			} else {
+				allProducts[key].checked.value = true;
+			};
+			console.log(allProducts[key].checked.value);
+			allProducts = Ti.App.Properties.setObject('allProducts', allProducts);
+		};
+	}
 }
 
+$.elementsList.addEventListener('itemclick', function(e) {
+	// TODO ecouter e, vérifier que l'élement cliquer est un ti.ui.switch puis enregistrer la valeur
+	console.log('test');
+	console.log(e);
+	console.log(new Date());
+});
 
 /* B O U T O N */
 $.validateProducts.addEventListener('click', function(e) {
